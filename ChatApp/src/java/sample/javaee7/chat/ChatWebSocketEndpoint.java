c780bec59e6f26ecc985c3cecfe7d5a584c532e5
tiikoino <oukakanose@gmail.com>
2014-12-19 13:26:26 +0900

    コメント追加

--- ChatApp/src/java/sample/javaee7/chat/ChatWebSocketEndpoint.java
+++ ChatApp/src/java/sample/javaee7/chat/ChatWebSocketEndpoint.java
@@ -16,16 +16,19 @@ public class ChatWebSocketEndpoint {
 
   @OnOpen
   public void onOpen(Session session) throws IOException {
+    //接続イベント処理
     sessions.add(session);
   }
 
   @OnClose
   public void onClose(Session session) throws IOException {
+    //クローズイベント処理
     sessions.remove(session);
   }
 
   @OnMessage
   public void onMessage(String message) {
+    //メッセージ受信イベント処理
     for (Session session : sessions) {
         session.getAsyncRemote().sendText(message);
     }
